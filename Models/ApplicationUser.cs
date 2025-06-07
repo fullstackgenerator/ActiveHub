55e172753b04dc4fdc0f543a7cae01a58ada6c3f
fullstackgenerator <simon.hojnik@gmail.com>
2025-06-07 10:32:22 +0200

    added registration model, updated applicationUser model

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -6,18 +6,31 @@ namespace ActiveHub.Models;
 public class ApplicationUser : IdentityUser
 {
     [Required]
+    [Display(Name = "First Name")]
+    [PersonalData]
     public string FirstName { get; set; }
     
     [Required]
+    [Display(Name = "Last Name")]
+    [PersonalData]
     public string LastName { get; set; }
     
+    [Required]
+    [Display(Name = "Date of Birth")]
     [DataType(DataType.Date)]
+    [PersonalData]
     public DateOnly DateOfBirth { get; set; }
     
+    [Required]
+    [PersonalData]
     public string Address { get; set; }
     
+    [Required]
     [Display(Name = "Postal Code")]
+    [PersonalData]
     public int PostNumber { get; set; }
     
+    [Required]
+    [PersonalData]
     public string City { get; set; }
 }
---
+++ Models/Registration.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ActiveHub.Models;
+
+public class Registration
+{
+    [Required]
+    [Display(Name = "Username")]
+    public string Username { get; set; }
+    
+    [Required]
+    [EmailAddress]
+    [Display(Name = "Email")]
+    public string Email { get; set; }
+    
+    [Required]
+    [DataType(DataType.Password)]
+    [Display(Name = "Password")]
+    [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+    public string Password { get; set; }
+    
+    [Required]
+    [DataType(DataType.Password)]
+    [Display(Name = "Confirm Password")]
+    [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+    public string ConfirmPassword { get; set; }
+    
+    [Required]
+    [Display(Name = "First Name")]
+    public string FirstName { get; set; }
+}
