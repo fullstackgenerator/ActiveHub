0ce6bd889bf12eb3bca6794d036ac17dacaa4cf3
fullstackgenerator <simon.hojnik@gmail.com>
2025-06-07 11:49:53 +0200

    updated registration model and controller

---
+++ Controllers/RegistrationController.cs
@@ -0,0 +1,52 @@
+﻿using ActiveHub.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ActiveHub.Controllers;
+
+public class RegistrationController : Controller
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly SignInManager<ApplicationUser> _signInManager;
+
+    public RegistrationController(
+        UserManager<ApplicationUser> userManager,
+        SignInManager<ApplicationUser> signInManager)
+    {
+        _userManager = userManager;
+        _signInManager = signInManager;
+    }
+
+    [HttpGet]
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Register(Registration model)
+    {
+        if (!ModelState.IsValid) return View(model);
+
+        var user = new ApplicationUser 
+        {
+            UserName = model.Username,
+            Email = model.Email,
+            FirstName = model.FirstName,
+            LastName = model.LastName
+        };
+    
+        var result = await _userManager.CreateAsync(user, model.Password);
+    
+        if (!result.Succeeded)
+        {
+            foreach (var error in result.Errors)
+                ModelState.AddModelError(string.Empty, error.Description);
+            return View(model);
+        }
+    
+        await _signInManager.SignInAsync(user, isPersistent: false);
+        return RedirectToAction("Index", "Home");
+    }
+}
--- Models/Registration.cs
+++ Models/Registration.cs
@@ -28,4 +28,8 @@ public class Registration
     [Required]
     [Display(Name = "First Name")]
     public string FirstName { get; set; }
+    
+    [Required]
+    [Display(Name = "Last Name")]
+    public string LastName { get; set; }
 }
